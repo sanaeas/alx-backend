1b7b282089c64a0c9c27339b9adc67308bf5a5a2
sanaeas <sanaassim1@gmail.com>
2024-03-04 10:57:07 +0100

    Node Redis client and advanced operations

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,31 @@
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (error) => {
+  console.error(`Redis client not connected to the server: ${error}`);
+});
+
+const createHash = () => {
+  client.hset('HolbertonSchools', 'Portland', 50, redis.print);
+  client.hset('HolbertonSchools', 'Seattle', 80, redis.print);
+  client.hset('HolbertonSchools', 'New York', 20, redis.print);
+  client.hset('HolbertonSchools', 'Bogota', 20, redis.print);
+  client.hset('HolbertonSchools', 'Cali', 40, redis.print);
+  client.hset('HolbertonSchools', 'Paris', 2, redis.print);
+};
+
+const displayHash = () => {
+  client.hgetall('HolbertonSchools', (err, reply) => {
+    if (reply) {
+      console.log(reply);
+    }
+  });
+};
+
+createHash();
+displayHash();
